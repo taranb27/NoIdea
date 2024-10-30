448bec8cc5d546e601ebb9428e01d88256a34f2b
taranb27 <taranboodhoo@gmail.com>
2024-10-30 09:59:26 +0400

    added new directories for dataset

--- main.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt 
-from tensorflow.keras import datasets, layers, models
-
-(training_images, training_labels), (testing_images, testing_labels) = datasets.cifer10.load_data()
-training_images, testing_images = training_images/255, testing_images/255
-
-class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Dog', 'Frog', 'Truck']

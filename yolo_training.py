b83d6cd4ee28591f32e9f3b93a435a78720854b4
taranb27 <taranboodhoo@gmail.com>
2024-10-30 21:07:56 +0400

    done

--- yolo_training.py
+++ yolo_training.py
@@ -1,12 +1,16 @@
 from ultralytics import YOLO
-# from IPython.display import display, Image
 
-model = YOLO("yolov8s.pt")
+# model = YOLO("yolov8s.pt")
+model = YOLO("runs/detect/train8/weights/best.pt")
 
-model.train(
-    data="/Users/tb/Desktop/Robotics/cw/NoIdea/Dataset/data.yaml",
-    epochs=25,
-    imgsz=224,
-    plots=True
+# model.train(
+#     data="/Users/tb/Desktop/Robotics/cw/NoIdea/Dataset/data.yaml",
+#     epochs=25,
+#     imgsz=224,
+#     plots=True
+# )
+
+model.predict(
+    conf=0.25,
+    source="Dataset/test/images"
 )
-

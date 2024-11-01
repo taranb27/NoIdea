695cf11fea9061551d0cd10e2152a60d9135f07e
deepika <db1224@live.mdx.ac.uk>
2024-11-01 09:33:26 +0400

    path modify

--- yolo_training.py
+++ yolo_training.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8s.pt")
 
 model.train(
-    data="/Users/tb/Desktop/Robotics/cw/NoIdea/new_dataset/data.yaml",
+    data="/Users/sneha/NoIdea/new_dataset/data.yaml",
     epochs=25,
     imgsz=640,
     plots=True
